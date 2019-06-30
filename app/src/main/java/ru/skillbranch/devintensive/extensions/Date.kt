18e75d9ef294686f479c3b9a4728a25a7336aa36
Lana <lana.bochkova2015@yandex.ru>
2019-06-30 20:05:17 +0300

    introduction into Kotlin. DTO User, Factory

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,30 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.format(pattern:String = "HH:mm:ss dd.MM.yy"): String {    //Ф-ия расширения принимает стандартный паттерн
+                                                                    // (в том виде,в кот. мы хотим отформатировать объект)
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value:Int, units:String): Date {   //extension ф-ия позволит добавить к объекту даты определенно временной сдвиг,
+                                                // кот. будет измеряться в задаваемыми нами величинами
+    var time = this.time
+
+    time += when(units) {
+        "second","seconds" -> value * SECOND
+        "minute","minutes" -> value * MINUTE
+        "hour","hours" -> value * HOUR
+        "day","days" -> value * DAY
+        else -> throw IllegalStateException("invalid unit")
+    }
+    this.time = time
+    return this
+}
