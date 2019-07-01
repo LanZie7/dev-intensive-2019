518bcbd3b60a4d7ef778adb496f7b06587792e6d
Lana <lana.bochkova2015@yandex.ru>
2019-07-01 21:36:55 +0300

    introduction into Kotlin. Date mapping, parseFullName

--- app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/UserView.kt
@@ -19,7 +19,4 @@ class UserView(
                 initials: $initials         
                 """.trimIndent())
         }
-
-
-
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -5,15 +5,17 @@ object Utils {
         val parts : List<String>? = fullName?.split(" ")
         var firstName = parts?.getOrNull(0)
         var lastName = parts?.getOrNull(1)
-//        return Pair (firstName, lastName)
-        return firstName to lastName    // Задается пара: ключ-значение
+        return Pair (firstName, lastName)
+//      return firstName to lastName    // Задается пара: ключ-значение?
     }
 
-    fun transliteration(payload: String, divider: String = " ") {
+    fun transliteration(payload: String, divider: String = " ") : String {
+        return " "
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
+        return " "
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
